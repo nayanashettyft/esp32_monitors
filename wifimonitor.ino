ceef5019e5d200efa3d4b25a8cc1caf4c6c61f08
Nayana Shetty <nayanashetty@nayanas-mbp.home>
2019-05-25 09:21:07 +0100

    added round trip time calculation for ping requests

--- wifimonitor.ino
+++ wifimonitor.ino
@@ -44,17 +44,47 @@ void setup()
 
 void loop()
 {
+  int success = 0;
+  int rttmax = 0;
+  int rttmin = -1;
+  int rtttotal = 0;
+  
   timeClient.update();
   unsigned long metric_time = timeClient.getEpochTime();
   Serial.println(metric_time);
-  
-  Serial.println("Pinging host ");
-  Serial.println(remote_host);
-  if(Ping.ping(remote_host)) {
-    Serial.println("Success!!");
-  } else {
-    Serial.println("Error :(");
+
+  // Get the avg round trip time
+  for (int i=0;i < 10;i++) {
+    if (Ping.ping(remote_host)) {
+      success++;
+      int rtt = Ping.averageTime();
+      if (rtt > rttmax) {
+        rttmax = rtt;
+      }
+      if (rttmin == -1 or rtt < rttmin) {
+        rttmin = rtt;
+      }
+      rtttotal += rtt;
+      Serial.println("Success!!");
+    } else {
+      Serial.println("Error :(");
+    }
   }
+
+  int rttavg = -1;
+  if (success > 0) {
+    rttavg = int(rtttotal / success);
+  }
+
+  int loss = (10 - success) * 10;
+  float rssi = WiFi.RSSI();
+
+  Serial.println(success);
+  Serial.println(rttavg);
+  Serial.println(rttmin);
+  Serial.println(rttmax);
+  Serial.println(loss);
+  Serial.println(rssi);
 
   delay(1000);
 }
