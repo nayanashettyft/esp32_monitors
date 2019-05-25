6f879ae93e9c79a3f001c0be48294dd3708dc186
Nayana Shetty <nayanashetty@nayanas-mbp.home>
2019-05-25 08:01:02 +0100

    first commit

---
+++ wifimonitor.ino
@@ -0,0 +1,54 @@
+#include <WiFiServerSecure.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiUdp.h>
+#include <ESP8266WiFiType.h>
+#include <CertStoreBearSSL.h>
+#include <ESP8266WiFiAP.h>
+#include <WiFiClient.h>
+#include <BearSSLHelpers.h>
+#include <WiFiServer.h>
+#include <ESP8266WiFiScan.h>
+#include <WiFiServerSecureBearSSL.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiSTA.h>
+#include <WiFiClientSecureAxTLS.h>
+#include <WiFiServerSecureAxTLS.h>
+
+#include <ESP8266Wifi.h>
+#include <ESP8266Ping.h>
+#include <NTPClient.h>
+
+char* ssid = "<WIFI address>";
+char* password = "<WIFI password>";
+const char* remote_host = "www.google.com";
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+  Serial.println("Connecting to WiFI");
+  WiFi.begin(ssid,password);
+  while(WiFi.status()!=WL_CONNECTED)
+  {
+    Serial.println(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Pinging host ");
+  Serial.println(remote_host);
+  if(Ping.ping(remote_host)) {
+    Serial.println("Success!!");
+  } else {
+    Serial.println("Error :(");
+  }
+}
+
+void loop()
+{
+}
